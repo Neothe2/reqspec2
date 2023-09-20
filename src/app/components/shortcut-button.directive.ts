6b08265463ff25c6e3d5d088d5731b0db465bf96
NeoThe2 <neo.j.jabin@gmail.com>
2023-09-20 21:45:56 +0530

    The shortcuts are working and highlight is working exept when you put in a letter which is not in the button

--- src/app/components/shortcut-button.directive.ts
+++ src/app/components/shortcut-button.directive.ts
@@ -27,17 +27,37 @@ export class ShortcutButtonDirective implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     const shortcut = this.el.nativeElement.getAttribute('keyboard-shortcut');
-    const buttonText = this.el.nativeElement.innerText;
-    const shortcutKey = shortcut.replace(/[()\[\]{}]/g, '');
-    this.shortcutSpan = this.renderer.createElement('span');
+    let buttonText = this.el.nativeElement.innerText;
+    const shortcutKey = (
+      shortcut?.replace(/[()\[\]{}]/g, '') || ''
+    ).toLowerCase();
 
-    if (buttonText.includes(shortcutKey)) {
-      this.renderer.setProperty(this.shortcutSpan, 'innerText', shortcutKey);
+    if (buttonText.toLowerCase().includes(shortcutKey)) {
+      const index = buttonText.toLowerCase().indexOf(shortcutKey);
+      const before = buttonText.substring(0, index);
+      const after = buttonText.substring(index + 1);
+      this.shortcutSpan = this.renderer.createElement('span');
+
+      this.renderer.setProperty(
+        this.shortcutSpan,
+        'innerText',
+        buttonText[index]
+      );
       this.renderer.addClass(
         this.shortcutSpan,
-        this.getShortcutClass(shortcut)
+        this.getShortcutClass(shortcut || '')
+      );
+
+      this.renderer.setProperty(this.el.nativeElement, 'innerHTML', '');
+      this.renderer.appendChild(
+        this.el.nativeElement,
+        this.renderer.createText(before)
       );
       this.renderer.appendChild(this.el.nativeElement, this.shortcutSpan);
+      this.renderer.appendChild(
+        this.el.nativeElement,
+        this.renderer.createText(after)
+      );
     }
 
     this.subscription = this.shortcutService.shortcut$.subscribe((keyInfo) => {
@@ -49,14 +69,28 @@ export class ShortcutButtonDirective implements AfterViewInit, OnDestroy {
 
   @HostListener('document:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent): void {
-    if (event.ctrlKey || event.altKey) {
-      this.renderer.addClass(this.shortcutSpan, 'active');
+    const shortcut = this.el.nativeElement.getAttribute('keyboard-shortcut');
+    const shortcutClass = this.getShortcutClass(shortcut || '');
+
+    if (this.shortcutSpan) {
+      if (
+        (shortcutClass === 'ctrl-shortcut-button' && event.ctrlKey) ||
+        (shortcutClass === 'alt-shortcut-button' && event.altKey) ||
+        (shortcutClass === 'ctrl-alt-shortcut-button' &&
+          event.ctrlKey &&
+          event.altKey)
+      ) {
+        event.preventDefault();
+        this.renderer.addClass(this.shortcutSpan, 'active');
+      }
     }
   }
 
   @HostListener('document:keyup', ['$event'])
   handleKeyUp(event: KeyboardEvent): void {
-    this.renderer.removeClass(this.shortcutSpan, 'active');
+    if (this.shortcutSpan) {
+      this.renderer.removeClass(this.shortcutSpan, 'active');
+    }
   }
 
   private getShortcutClass(shortcut: string): string {
