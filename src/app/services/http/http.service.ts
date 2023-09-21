61dd35d69ba1f5bcc5d709566de239de8382e892
NeoThe2 <neo.j.jabin@gmail.com>
2023-09-21 10:41:11 +0530

    electron is correctly being served

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,14 +1,55 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+
+declare global {
+  interface Window {
+    require: any;
+    process: any;
+  }
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
   private baseUrl = 'http://127.0.0.1:8000';
+  private isElectron: boolean;
 
-  constructor(private http: HttpClient, private cookieService: CookieService) {}
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
+    @Inject(PLATFORM_ID) platformId: string
+  ) {
+    this.isElectron = window && window.process && window.process.type;
+  }
+
+  getCookie(key: string): string | null {
+    if (this.isElectron) {
+      return this.cookieService.get(key);
+    } else {
+      let value = localStorage.getItem(key);
+      return value;
+    }
+  }
+
+  setCookie(key: string, value: string): void {
+    if (this.isElectron) {
+      this.cookieService.set(key, value);
+    } else {
+      localStorage.setItem(key, value);
+    }
+  }
+
+  deleteCookie(key: string): void {
+    if (this.isElectron) {
+      this.cookieService.delete(key);
+    } else {
+      localStorage.removeItem(key);
+    }
+  }
 
   login(username: string, password: string) {
     const body = { username, password };
@@ -17,8 +58,8 @@ export class HttpService {
       .toPromise()
       .then((response: any) => {
         if (response && response.access && response.refresh) {
-          this.cookieService.set('access', response.access);
-          this.cookieService.set('refresh', response.refresh);
+          this.setCookie('access', response.access);
+          this.setCookie('refresh', response.refresh);
         } else {
           throw new Error('Access or refresh token is missing in the response');
         }
@@ -35,7 +76,7 @@ export class HttpService {
       .toPromise()
       .then((response: any) => {
         if (response && response.access) {
-          this.cookieService.set('access', response.access);
+          this.setCookie('access', response.access);
         } else {
           throw new Error('Access token is missing in the response');
         }
@@ -44,7 +85,7 @@ export class HttpService {
 
   logout() {
     // Example: Remove the access token from cookies
-    this.cookieService.delete('access');
+    this.deleteCookie('access');
     // this.cookieService.delete('refresh');
 
     // Example: Make an HTTP request to the logout endpoint
@@ -54,13 +95,13 @@ export class HttpService {
   }
 
   get(url: string) {
-    const access = this.cookieService.get('access');
+    const access = this.getCookie('access');
     const headers = new HttpHeaders().set('Authorization', `JWT ${access}`);
     return this.http.get(`${this.baseUrl}/${url}`, { headers }).toPromise();
   }
 
   post(url: string, body: any) {
-    const access = this.cookieService.get('access');
+    const access = this.getCookie('access');
     const headers = new HttpHeaders().set('Authorization', `JWT ${access}`);
     return this.http
       .post(`${this.baseUrl}/${url}`, body, { headers })
@@ -68,7 +109,7 @@ export class HttpService {
   }
 
   put(url: string, body: any) {
-    const access = this.cookieService.get('access');
+    const access = this.getCookie('access');
     const headers = new HttpHeaders().set('Authorization', `JWT ${access}`);
     return this.http
       .put(`${this.baseUrl}/${url}`, body, { headers })
@@ -76,7 +117,7 @@ export class HttpService {
   }
 
   patch(url: string, body: any) {
-    const access = this.cookieService.get('access');
+    const access = this.getCookie('access');
     const headers = new HttpHeaders().set('Authorization', `JWT ${access}`);
     return this.http
       .patch(`${this.baseUrl}/${url}`, body, { headers })
